b38d6028af3b1b1a1e3be7dd5d6e605435c22e9a
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-30 12:37:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,71 +1,78 @@
+
+
 class Song
-  attr_accessor :name, :artist_name
-  @@all = []
-
-  def self.all
-    @@all
-  end
-
-  def save
-    self.class.all << self
-  end
-
-def self.create
-  song = Song.new
-  song.save
-  song
-end
-
-def self.new_by_name(song_name)
-  song = Song.new
-  song.name = song_name
-  song
-end
-
-def self.create_by_name(song_name)
-  song = Song.create
-  song.name = song_name
-  song
-end
-
-def self.find_by_name(song_name)
-    @@all.find { |song|
-      song.name == song_name
-    }
-  end
-
-def self.find_or_create_by_name(song_name)
-    self.find_by_name(song_name) || self.create_by_name(song_name)
-  end
-  
-  def self.alphabetical
-    @@all.sort_by { |song|
-      song.name
-    }
-  end
-  
-  def self.new_from_filename(filename)
-    new_file = filename.split(" - ") 
-    artist_name = new_file[0]
-    song_name = new_file[1].gsub(".mp3","")
-    song = self.new
-    song.name = song_name
-    song.artist_name = artist_name
-    song
-  end
-
-  def self.create_from_filename(filename)
-    new_file = filename.split(" - ") 
-    artist_name = new_file[0]
-    song_name = new_file[1].gsub(".mp3","")
-    song = self.create
-    song.name = song_name
-    song.artist_name = artist_name
-    song
-  end
-
-  def self.destroy_all
-    @@all.clear
-  end
+  attr_accessor :name
   
 end
+
+# class Song
+#   attr_accessor :name, :artist_name
+  
+#   @@all = []
+  
+#   def self.create
+#     song = Song.new
+#     song.save
+#     song
+#   end
+
+#   def save
+#     @@all << self
+#   end
+
+# def self.all
+#   @@all
+# end
+ 
+# def self.new_by_name(name)
+#   song = Song.new
+#   song.name = name
+#   song
+# end
+ 
+# def self.create_by_name(name)
+#   song = Song.new
+#   song.name = name
+#   song.save
+#   song
+# end
+ 
+# def self.find_by_name(name)
+#   @@all.find{|song| song.name == name}
+# end
+ 
+# def self.find_or_create_by_name(name)
+   
+#   self.find_by_name(name) || self.create_by_name(name)
+#   end
+   
+#   def self.alphabetical
+#     @@all.sort_by {|song| song.name}
+#   end
+ 
+# def self.new_from_filename(filename)
+#   new_file = filename.split(" - ")
+#   artist_name = new_file[0]
+#   song_name= new_file[1].gsub(".mp3","")
+#   song = Song.new
+#   song.name = song_name
+#   song.artist_name = artist_name
+#   song
+# end
+ 
+# def self.create_from_filename(filename)
+#   new_file = filename.split(" - ")
+#   artist_name = new_file[0]
+#   song_name= new_file[1].gsub(".mp3","")
+#   song = self.create
+#   song.name = song_name
+#   song.artist_name = artist_name
+#   song
+# end
+ 
+# def self.destroy_all
+#   @@all.clear
+   
+# end
+ 
+# end
